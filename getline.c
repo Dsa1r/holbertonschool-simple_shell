35cacee557fc7a2f98febc5b70764f761875f43d
dsa1r <danamusaad22@gmail.com>
2026-05-16 13:13:29 +0000

    fix error of getline function

--- getline.c
+++ getline.c
@@ -3,9 +3,9 @@
 #define BUFFER_SIZE 1024
 
 /**
- * _getline - custom getline using read
- * @lineptr: pointer to buffer
- * @n: size of buffer
+ * _getline - custom getline without realloc
+ * @lineptr: buffer
+ * @n: size
  *
  * Return: number of chars read
  */
@@ -14,9 +14,8 @@ ssize_t _getline(char **lineptr, size_t *n)
 	static char buffer[BUFFER_SIZE];
 	static ssize_t index, bytes_read;
 	ssize_t i = 0;
-	char *new_line;
 
-	if (*lineptr == NULL || *n == 0)
+	if (*lineptr == NULL)
 	{
 		*n = BUFFER_SIZE;
 		*lineptr = malloc(*n);
@@ -40,15 +39,9 @@ ssize_t _getline(char **lineptr, size_t *n)
 		if ((*lineptr)[i - 1] == '\n')
 			break;
 
-		/* expand buffer if needed */
-		if (i >= (ssize_t)(*n - 1))
-		{
-			*n *= 2;
-			new_line = realloc(*lineptr, *n);
-			if (!new_line)
-				return (-1);
-			*lineptr = new_line;
-		}
+
+		if (i >= BUFFER_SIZE - 1)
+			break;
 	}
 
 	(*lineptr)[i] = '\0';
